2727d713a730f86bcbd8510417f1835148f69192
TUDO 3D <tudo3dmichel@gmail.com>
2025-08-08 07:44:52 -0400

    Delete jquery.js

--- jquery.js
+++
@@ -1,8 +0,0 @@
-    let slideIndex = 0;
-    const carousel = document.getElementById("carousel");
-    const totalSlides = carousel.children.length;
-
-    function moveSlide(step) {
-      slideIndex = (slideIndex + step + totalSlides) % totalSlides;
-      carousel.style.transform = `translateX(-${slideIndex * 100}%)`;
-    }
